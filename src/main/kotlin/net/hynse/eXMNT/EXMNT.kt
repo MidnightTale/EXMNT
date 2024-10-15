6e5c73cb0f1a66c2d64c257c45544d6f00681d6d
MidnightTale <midnighttale24@gmail.com>
2024-10-15 11:05:39 +0700

    Initial commit

---
+++ src/main/kotlin/net/hynse/eXMNT/EXMNT.kt
@@ -0,0 +1,14 @@
+package net.hynse.eXMNT
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class EXMNT : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
